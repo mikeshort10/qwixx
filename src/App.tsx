4e73efb1819364ee94dfcedae92e23eb72bc7db2
Michael Short <mshort@dhcp-103-166.in.aps.org>
2020-01-13 18:22:02 -0500

    resave

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ const App: React.FC = () => {
         showScores={showScores}
         statuses={colors}
         setStatuses={setColors}
-        setStatusOpen={selectNumber}
+        setStatusOpen={selectNumberFromDice}
       />
       <div className="flex mt-2">
         <div className="flex">
